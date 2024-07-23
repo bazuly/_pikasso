f7565c64aa14b10eb8a19e2169bb678c9aa6edf0
Сергей Балиоз <serejka50@gmail.com>
2024-07-23 15:36:39 +0300

    swagger update

--- pikasso/settings.py
+++ pikasso/settings.py
@@ -28,7 +28,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'bike_rental',
     'drf_yasg',
-    
+    'rest_framework_simplejwt',
+       
 ]
 
 MIDDLEWARE = [
@@ -120,29 +121,21 @@ REST_FRAMEWORK = {
     ),
 }
 
-SIMPLE_JWT = {    
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
     'ALGORITHM': 'HS256',
     'SIGNING_KEY': SECRET_KEY,
     'VERIFYING_KEY': None,
-    'AUDIENCE': None,
-    'ISSUER': None,
-    'JWK_URL': None,
-    'LEEWAY': 0,
-
     'AUTH_HEADER_TYPES': ('Bearer',),
-    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
     'USER_ID_FIELD': 'id',
     'USER_ID_CLAIM': 'user_id',
-    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
-
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
     'TOKEN_TYPE_CLAIM': 'token_type',
-    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
-
     'JTI_CLAIM': 'jti',
-
     'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=300),
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 
@@ -157,3 +150,17 @@ CELERY_RESULT_SERIALIZER = 'json'
 
 REDIS_BROKER_URL = "redis://redis:6379/0"
 REDIS_RESULT_BACKEND = "redis://redis:6379/0"
+
+
+# swagger settings 
+
+SWAGGER_SETTINGS = {
+   # 'USE_SESSION_AUTH': False,
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+        }
+    },
+}
